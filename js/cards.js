bfd19f1a1480e2f2a993424478de3cf4ed6ca71f
min7 <min7@pdx.edu>
2021-06-05 20:25:58 -0700

    Added comments to card.js in displayCards() function, function needed comments for clarity, closes #5

--- js/cards.js
+++ js/cards.js
@@ -1,27 +1,34 @@
+//API URL
 let url = "https://akabab.github.io/superhero-api/api/all.json";
 let cardsInfo = document.getElementById('cards');
 
 window.onload = getApiJson();
 
+//display cards for each superhero
 function displayCards(data) {
+
+    //loop through array with data and display each hero info to DOM
     data.forEach(element => {
+        //create <div> element. Add all card info to this div element.
         let div = document.createElement('div');
-        // div.className = 'card-border';
         cardsInfo.appendChild(div);
-        // let a = document.createElement('a');
+        //create <img> element in DOM. Add image src and alt to img element 
         let img = document.createElement('img');
         img.src = element.images.sm;
         img.alt = element.name;
         div.appendChild(img);
+        //create <h5> element. Add hero name and name styling to h5
         let name = document.createElement('h5');
         name.setAttribute('width', '160');
         name.className = 'heroName';
         div.appendChild(name);
         name.innerText = element.name;
+        //create <p> element. Add hero's real name and styling to <p>
         let realName = document.createElement('p');
         realName.setAttribute('width', '160');
         div.appendChild(realName);
 
+        //display the hero's fullname, if they have one
         if(element.biography.fullName === '') {
             realName.innerText = element.name;
         } else {
@@ -30,6 +37,7 @@ function displayCards(data) {
     })
 }
 
+//get API data in JSON format
 async function getApiJson() {
     let getApi = await fetch(url);
 
@@ -43,6 +51,7 @@ async function getApiJson() {
         console.log('error');
     }
 
+    //function call to display all cards using JSON data
     displayCards(getJson);
 
 }
